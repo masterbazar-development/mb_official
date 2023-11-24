e2e1e8ed29e08193e9a5af4addcc345fa5886e46
karan0000000 <karan.masterbazar@gmail.com>
2023-11-24 17:58:11 +0530

    karan testimonials changes

---
+++ mb-admin/testimonial/client-testimonial.php
@@ -0,0 +1,103 @@
+<?php
+include('../authentication.php');
+include('../includes/header.php');
+include('../includes/navbar-top.php');
+
+if (isset($_POST['submit'])) {
+    // Handle the file upload
+    $image = $_FILES['image'];
+    $image_name = $image['name'];
+    $image_tmp = $image['tmp_name'];
+    $image_size = $image['size'];
+    $image_error = $image['error'];
+  
+    // Specify the directory to which the image will be uploaded
+    $upload_directory = '../images/blog/';
+  
+    // Generate a unique name for the image
+    $image_path = $upload_directory . uniqid() . '_' . $image_name;
+  
+    // Move the uploaded image to the desired location
+    move_uploaded_file($image_tmp, $image_path);
+  
+    // Get the Summernote content from the textarea
+    $content = $_POST['content'];
+  
+    // Replace the image source in the Summernote content with the uploaded image path
+    $content = str_replace('src="data:image', 'src="' . $image_path . '" data-filename="image', $content);
+  
+    // Use the modified content as needed (e.g., save it to the database, display it, etc.)
+   
+    // You can also store the image path in the database if needed
+    // $query = "INSERT INTO posts (content, image_path) VALUES ('$content', '$image_path')";
+    // mysqli_query($connection, $query);
+  
+    // Redirect or perform any other necessary actions
+    // header('Location: your-page.php');
+    exit();
+  } 
+  $content = '';
+// Sample string with Base64 data
+// $content = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. <img src="data:image/png;base64,/9j/4AAQSkZJRgABAQAAAQABAAD...">';
+
+// Regular expression pattern to match Base64 data
+$pattern = '/src="data:image\/[^;]+;base64,([^"]+)"/';
+
+// Replace Base64 data with actual image data
+$replacedContent = preg_replace_callback($pattern, function($matches) {
+    $base64Data = $matches[1];
+    $imageData = base64_decode($base64Data);
+    $filename = 'image.jpg'; // Provide a filename for the image
+    file_put_contents($filename, $imageData);
+    return 'src="' . $filename . '"';
+}, $content);
+
+// Output the replaced content
+echo $replacedContent;
+?>
+
+<div class="container-fluid px-4">
+    <div class="row my-4">
+        <div class="col-md-12">
+            <?php include('../message.php'); ?>
+            <div class="card">
+                <div class="card-header">
+                    <h4>ADD TESTIMONIALS
+                        <a href="testimonial/view-testimonials" class="btn btn-danger float-end">Back</a>
+                    </h4>
+                </div>
+                <div class="card-body">
+
+                    <form action="../code.php" method="POST" enctype="multipart/form-data">
+                    
+                        <div class="row">
+                            <div class="col-md-6 mb-3">
+                                <lable for="">Client Name</lable>
+                                <input type="text" name="client-name" class="form-control">
+                            </div>
+                            <div class="col-md-6 mb-3">
+                                <lable for="">Comment Heading</lable>
+                                <input type="text" name="comment-head" class="form-control">
+                            </div>
+                            <div class="col-md-12 mb-3">
+                                <lable for="">Comment</lable>
+                                <textarea name="comment" class="form-control" rows="4"></textarea>
+                            </div>
+                            <div class="col-md-6 mb-3">
+                                <lable for="">Client Image</lable>
+                                <input type="file" name="client-image" class="form-control">
+                            </div> 
+                            <div class="col-md-12 mb-3">
+                                <button type="submit" name="testimonial_add" class="btn btn-primary">Add Client Testimonials</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+include('../includes/footer.php')
+?>
--- mb-admin/testimonial/testimonial-add.php
+++ mb-admin/testimonial/testimonial-add.php
@@ -11,30 +11,40 @@ include('../includes/navbar-top.php')
             <?php include('../message.php'); ?>
             <div class="card">
                 <div class="card-header">
-                    <h4>Add Testimonial
+                    <h4>ADD Category
                         <a href="view-register" class="btn btn-danger float-end">Back</a>
                     </h4>
                 </div>
                 <div class="card-body">
-                    <form action="../code" method="POST" enctype="multipart/form-data">
-                        <div class="row"><div class="col-md-4 mb-3">
-                                <lable for="">Client Name</lable>
-                                <input type="text" name="name" class="form-control" required>
+                    <form action="code.php" method="POST">
+                        <div class="row">
+
+                            <div class="col-md-4 mb-3">
+                                <lable for="">Select Main</lable>
+                                <select name="select-main" class="form-control">
+                                <option invalid>Select your main category</option>
+                                    <option>BLOG</option>
+                                    <option>FAQ</option>
+                                    <option>TESTIMONIAL</option>
+                                    <option>REVIEW</option>
+                                    <option>CLIENT's LOGO</option>
+                                    <option>PROJECTS</option>
+                                </select>
                             </div>
-                            <div class="col-md-3 mb-3">
-                                <lable for="">Image Upload</lable>
-                                <input type="file" name="image" class="form-control" required>
+                            <div class="col-md-4 mb-3">
+                                <lable for="">Name of Sub Category</lable>
+                                <input type="text" name="name" class="form-control">
                             </div>
-                            <div class="col-md-5 mb-3">
-                                <lable for="">Comment Heading</lable>
-                                <input type="text" name="com-head" class="form-control" required>
+                            <div class="col-md-4 mb-3">
+                                <lable for="">Slug</lable>
+                                <input type="text" name="slug" class="form-control">
                             </div>
                             <div class="col-md-12 mb-3">
-                                <lable for="">Comment</lable>
-                                <textarea name="comment" class="form-control" rows="4"></textarea>
+                                <lable for="">Description</lable>
+                                <textarea name="description" class="form-control" rows="4"></textarea>
                             </div>
                             <div class="col-md-6 mb-3">
-                                <lable for="">Navbar status</lable>
+                                <lable for="">Navbar Status</lable>
                                 <input type="checkbox" name="navbar_status" width="70px" height="70px" />
                             </div>
                             <div class="col-md-6 mb-3">
@@ -42,10 +52,12 @@ include('../includes/navbar-top.php')
                                 <input type="checkbox" name="status" width="70px" height="70px" />
                             </div>
                             <div class="col-md-12 mb-3">
-                                <button type="submit" name="testimonial_add" class="btn btn-primary">Add Testimonial</button>
+                                <button type="submit" name="category_add" class="btn btn-primary">Save Sub Category</button>
                             </div>
                         </div>
                     </form>
+
+
                 </div>
             </div>
         </div>
