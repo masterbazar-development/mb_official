9a7401390f4f422b5d1274f704103904391698ba
karan0000000 <karan.masterbazar@gmail.com>
2023-11-30 13:20:57 +0530

    heroheader

---
+++ components/heroheader.php
@@ -0,0 +1,17 @@
+<header class="flex justify-between items-center px-[10vw] absolute left-0 top-3 w-full z-[110]">
+    <div class="flex justify-start items-center gap-2 py-4">
+        <img src="<?php echo $mainUrl; ?>assets/client/images/masterbazar_logo.png" class="2xl:w-20 w-16 2xl:h-20 h-16" alt="masterbazar-logo">
+        <div class="">
+            <h4 class="2xl:text-4xl text-3xl text-head font-extrabold">Master<span class="text-[#264CC5]">Bazar</span></h4>
+            <p class="text-xs text-head font-medium text-center">Masters of the digital world!</p>
+        </div>
+    </div>
+
+    <div class="hidden lg:block flex justify-center items-center gap-4">
+        <button role="button" class="mx-auto w-max py-3 px-8 font-bold text-head bg-gradient-to-br from-[#2563EB33] to-[#7DA5FB33] rounded-full flex justify-center gap-4 items-center">
+            <span>Let's Have A Talk!
+            </span>
+            <span><i class="fa-solid fa-message text-xl text-blue"></i></span>
+        </button>
+    </div>
+</header>
