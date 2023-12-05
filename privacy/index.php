4636e02de2513670ccbf15daa3a389d2ff37f2fe
Femur242 <priyanshi.masterbazar@gmail.com>
2023-12-05 06:15:34 +0530

    priya

---
+++ privacy/index.php
@@ -0,0 +1,67 @@
+<?php include('../components/header.php'); ?>
+<?php include('../components/loader.php'); ?>
+<?php include('../components/navmenu.php'); 
+?>
+
+<!-- Ellipse Starts From Here -->
+<img src="<?php echo $mainUrl; ?>assets/client/images/ellipse.png" class="hidden xl:block fixed -top-60 sm:-right-40 -right-40 z-[95]">
+<!-- Ellipse ends Here -->
+
+<!-- Navigation starts from Here -->
+<?php include('../components/menu.php'); ?>
+<!-- Navigation ends Here -->
+
+<div class="justify-center flex ">
+    <div class="container mx-auto max-w-7xl px-0">
+        <?php include('../components/heroheader.php'); ?>
+    </div>
+</div>
+
+<div  style=" height:100px" aria-hidden = "true" class="wp-block-spacer swipe slide"></div>
+
+<div class="container text-head mx-auto max-w-7xl">
+    <h1 class=" text-center  xl:text-5xl text-4xl text-head font-extrabold pt-14 mb-3 md:mb-6 "> Privacy Policy </h1>
+
+    <p class="text-base lg:text-lg font-livvic  mt-5 text-[#334155] w-full px-5" > Our privacy policy governs how MasterBazar uses, collects, maintains, and discloses information collected from users. This privacy policy applies to the site and all products as well as services offered by MasterBazar.</p>
+<p class="text-base lg:text-lg font-livvic  mt-3 text-[#334155] w-full px-5">We respect everyone who visits our website. Our top priority is the security of your personal information. By submitting your personal information, you give us permission to use it in accordance with the policy. We strongly advise you to carefully read the privacy statement before providing any information.</p>
+<p class="text-base lg:text-lg font-livvic  mt-3 text-[#334155] w-full px-5">By using the site or supplying us with private details, you consent to MasterBazar collecting, using, and disclosing that information. It also means that you agree to all of the terms of this Privacy Statement.</p>
+<h3 class="text-3xl text-head font-extrabold text-left pt-5 px-5">What Information We Take</h3>
+<p class="text-base lg:text-lg font-livvic  mt-5 text-[#334155] w-full px-5">We may gather the following data:</p>
+<ul class="font-livvic font-normal mt-5 text-[#334155] px-10 text-base lg:text-lg">
+    <li class="list-disc">name and title,</li>
+    <li class="list-disc">contact information, including email address, demographic information, such as postcode, preferences, and interests,</li>
+    <li class="list-disc">other information relevant to customer surveys and/or offers</li>
+    <li class="list-disc">Your public persona</li>
+</ul>
+<h3  class="text-3xl text-head font-extrabold text-left pt-5 px-5"> How We Use the Details That We Collect</h3>
+<ul class="text-base lg:text-lg font-livvic font-normal mt-5 text-[#334155] px-10">
+ <li class="list-disc">Improving the quality of our services and products.</li>
+ <li class="list-disc">The information you provide allows us to more efficiently respond to your customer service and support their needs.</li>
+ <li class="list-disc">To customize the user experience.</li>
+ <li class="list-disc">We use aggregate information to better understand how our users use the services and resources on our site.</li>
+ <li class="list-disc">To share information about our new services, products, special deals, and so on that we believe you will find useful or interesting.</li>
+ <li class="list-disc">To conduct market research in order to understand your interests and preferences and tailor our website accordingly.</li>
+</ul>
+
+<h3 class="text-3xl text-head font-extrabold text-left px-5 pt-6">Cookies</h3>
+<p class="text-base lg:text-lg font-livvic mt-5 text-[#334155] px-5">A cookie is a small file that requests permission to be stored on your computer's hard drive. Once you agree, the file is added, and the cookie assists in web traffic analysis or notifies you when you visit a specific site. Cookies enable web applications to recognize and respond to you as an individual. By assembling and recalling things about your choices, the web service can customize its operations to your needs, preferences, and dislikes. </p>
+<p class="text-base lg:text-lg font-livvic mt-5 text-[#334155] px-5">To determine which pages are being accessed, we use traffic log cookies. This allows us to analyse data about website traffic and enhance our website to better meet our customers' needs. We only use this data for statistical analysis and then delete it from the system. </p>
+<p class="text-base lg:text-lg font-livvic mt-5 text-[#334155] px-5">You have the option of accepting or declining cookies. Most web browsers accept cookies automatically, but you can usually change your browser settings to refuse cookies if you choose. This may restrict you from getting the benefit of the website.</p>
+<h3 class="text-3xl text-head font-extrabold text-left px-5 pt-6">Links to Other Websites</h3>
+<p class="text-base lg:text-lg font-livvic mt-5 text-[#334155] px-5"> Our website may contain links to relevant third-party websites. However, once you use these links to leave our site, please keep in mind that we have no power over that other website. As a result, we cannot guarantee the security and privacy of any data you offer while going to visit such sites, and such sites are not covered by this privacy statement. You should proceed with caution and carefully read the privacy statement for the website in question.</p>
+
+<h3 class="text-3xl font-extrabold text-left px-5 pt-6"> How You Can Control Your Information?</h3>
+<p class="text-base lg:text-lg font-livvic text-[#334155] px-5 pt-4">You have the following options for restricting the collection or use of your personal information: </p>
+<ul class="text-base lg:text-lg font-livvic font-normal mt-5 text-[#334155] px-10">
+    <li class="list-disc">When filling out a form on the website, look for the box that allows you to indicate that you do not want the information to be used for direct marketing purposes.</li>
+    <li class="list-disc">If you have mutually promised to us using your individual information for direct marketing purposes, you may shift your mind all the time by composing to or emailing us at</li>
+    <li class="list-disc">We will not sell, distribute, or lease your personal information to third parties without your permission or unless required by law. We may send you promotional information using your personal information.</li>
+</ul>
+
+<h3 class="text-3xl font-extrabold text-left px-5 pt-6">Contact Us</h3>
+<p class="text-base lg:text-lg font-livvic text-[#334155] px-5 pt-4">You can contact us if you have any complaints regarding our privacy policy at our email address and phone number.</p>
+</div>
+
+
+<?php include('../components/cursor.php'); ?>
+<?php include('../components/footer.php'); ?>
---
+++ terms/index.php
@@ -0,0 +1,61 @@
+<?php include('../components/header.php'); ?>
+<?php include('../components/loader.php'); ?>
+<?php include('../components/navmenu.php'); 
+?>
+
+<!-- Ellipse Starts From Here -->
+<img src="<?php echo $mainUrl; ?>assets/client/images/ellipse.png" class="hidden xl:block fixed -top-60 sm:-right-40 -right-40 z-[95]">
+<!-- Ellipse ends Here -->
+
+<!-- Navigation starts from Here -->
+<?php include('../components/menu.php'); ?>
+<!-- Navigation ends Here -->
+
+<div class="justify-center flex ">
+    <div class="container mx-auto max-w-7xl px-0">
+        <?php include('../components/heroheader.php'); ?>
+    </div>
+</div>
+
+<div  style=" height:100px" aria-hidden = "true" class="wp-block-spacer swipe slide"></div>
+<div class="container text-head mx-auto max-w-7xl">
+<h1 class=" text-center 2xl:text-6xl text-5xl text-head font-extrabold pt-14 mb-3 md:mb-10 pb-8 "> Terms And Conditions </h1>
+<p class="text-base lg:text-lg font-livvic  mt-5 text-[#334155] w-full px-5"> By accessing this website, you agree to be bound by its Terms and Conditions of Use, all applicable laws and regulations, and that you are responsible for complying with any applicable local laws. You may not use or access this site if you do not agree with any of these terms. This website's materials are protected by copyright and trademark law.</p>
+
+<h3 class="text-3xl text-head font-extrabold text-left pt-5 px-5"> License Grant</h3>
+<p class="text-base lg:text-lg font-livvic  mt-5 text-[#334155] w-full px-5">Company henceforth gives you a limited, non-exclusive, and non-transferable license to register with, access, and use the Platform strictly in accordance with the terms of this Agreement, including any Additional Terms, subject to the terms of this Agreement (defined below). You agree and acknowledge that You are solely responsible and liable for Your use of the Website and Platform, as well as for providing or obtaining, as well as maintaining all hardware, software, electrical power, telecommunications, Internet services, and other products or services required or desirable for You to access and use the Platform or the Website.</p>
+
+<h3 class="text-3xl text-head font-extrabold text-left pt-5 px-5">Restrictions</h3>
+<ul class="text-base lg:text-lg font-livvic font-normal mt-5 text-[#334155] px-10"> 
+    <li class="list-disc">You Shall Not: Alter, translate, make adjustments, or otherwise prepare derivative works or advancements of the Platform or Website, whether or not patentable;</li>
+<li class="list-disc">Reverse engineer, disassemble, decompile, decode, or otherwise attempt to derive or gain access to the Platform or Website's or any part thereof's source code.</li>
+<li class="list-disc">Erase, destroy, alter, or conceal any trademarks or other intellectual property or proprietary rights notices from the Platform or Website, along with any copy thereof;</li>
+<li class="list-disc">Rent, lease, lend, sell, license, transfer, publish, publish, transmit, or otherwise make accessible to any third party the Platform or Website or any of its features or functionality for any reason;</li>
+<li class="list-disc">Delete, disable, circumvent, or otherwise start creating or enforce any temporary fix to any copy protection, content protection, or security features in or protecting the Platform or Website;</li>
+<li class="list-disc">Use the Website or Platform or any part thereof for any purpose that:
+    <ol class="list-decimal font-livvic font-normal mt-5 text-[#334155] px-8">
+        <li>Tries to interfere with or provoke a breach of the contractual relationships between the Company and its employees;</li>
+<li>Is unlawful or prohibited, or is harmful or destructive to anyone or their property;</li>
+ <li>Is harmful or destructive to anyone or their property;</li> 
+ <li>Ends an unsolicited email to this site or to anyone whose email address includes the domain name listed on the Website;</li>
+ <li>Disrupts the Company's network services;</li>
+ <li>Attempting to gain unauthorized access to the Company's network services;</li>
+ <li>Implying an express or implied affiliation with the Company or a broker relationship with the Company (without the Company's express written permission);</li>
+<li>Impairs or limits the Company's or any other person's ability to access and use the Platform or Website;</li>
+</ol>   
+     </li>    
+</ul>
+
+<h3 class="text-3xl text-head font-extrabold text-left pt-5 px-5">Intellectual Property and Reservation Of Rights</h3>
+<p class="text-base lg:text-lg font-livvic mt-5 text-[#334155] w-full px-5">The Platform is provided to You under license rather than sale; and
+You do not acquire any ownership interest in the Platform or Website under this Agreement or any other rights thereto, other than the right to use the Platform in accordance with the license granted and access the Website, both of which are subject to all terms, conditions, and restrictions set forth in this Agreement. Except as expressly granted to You in this Agreement, Company, its licensors, and service providers reserve and shall retain all right, title, and interest in and to the Website and Platform, including all copyrights, trademarks, and other intellectual property rights therein or relating thereto.</p>
+
+<h3 class="text-3xl text-head font-extrabold text-left pt-5 px-5">Disclaimer</h3>
+<p class="text-base lg:text-lg font-livvic mt-5 text-[#334155] w-full px-5">The information on MasterBazar’s website is provided " as is." MasterBazar makes no express or implied warranties and expressly disclaims and negates all other warranties, including, but not limited to, implied warranties or conditions of merchantability, fitness for a particular purpose, or non-infringement of intellectual property or other rights. Furthermore, MasterBazar makes no warranties or representations about the accuracy, likely results, or reliability of the materials on its Internet website or otherwise relating to such materials or on any sites linked to this site.</p>
+
+<h3  class="text-3xl text-head font-extrabold text-left pt-5 px-5">Modification of terms and conditions</h3>
+<p class="text-base lg:text-lg font-livvic mt-5 text-[#334155] w-full px-5">MasterBazar reserves the right to revise these terms of service for its website at any time and without notice. By using this website, you agree to be bound by the then-current version of these Terms and Conditions of Use.</p>
+</div>
+
+<?php include('../components/cursor.php'); ?>
+<?php include('../components/footer.php'); ?>
